c9ea088cab92546e9139d64d49b5bc2552b63ac6
shoji_hiroya <uncut8@shoujiyuuyanoMacBook-Pro.local>
2018-08-07 10:45:55 +0900

    Edit-routes.rb-create_search

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+
+  resources :users, only: [:edit, :update] do
+    collection do
+      get :search
+    end
+  end
+
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
