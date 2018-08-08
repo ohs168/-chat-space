979a22a5e9e2b3426820976aed486caf3fb3a0f0
shoji_hiroya <uncut8@shoujiyuuyanoMacBook-Pro.local>
2018-08-08 17:14:31 +0900

    Add incremental-search

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,68 @@
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    var searchResult = $('#user-search-result'); //userの検索結果
+    var addedUsers = $('#chat-group-users'); //追加されたuser
+
+    function appendUser(user) {//インクリメンタルサーチの結果を表示させる関数
+      var html =
+      `<div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${ user.name }</p>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+       </div>`
+      searchResult.append(html);
+    }
+
+    function appendNoUser(user) { //該当userがいない場合の関数
+      var html =
+      `<div class="chat-group-user clearfix">
+         <p>${ user }</p>
+       </div>`
+       searchResult.append(html);
+    }
+
+    function appendToMember(userName, userId) { //検索結果からチャットメンバーに追加する関数
+      var html =
+      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ userId }'>
+       <input name='group[user_ids][]' type='hidden' value='${ userId }'>
+       <p class='chat-group-user__name'>${ userName }</p>
+       <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
+      addedUsers.append(html)
+    }
+
+    $("#user-search-field").on('keyup', function(){ //検索窓に入力された時発火
+      var input = $.trim($(this).val()); //文字列の両端の空白を削除
+      $.ajax({
+        type: 'GET',
+        url: '/users/search',
+        data: { keyword: input },
+        dataType: 'json',
+      })
+      .done(function(users){
+        $('#user-search-result').empty(); //要素内の子要素(テキストも対象)を全て削除
+        if (users.length !== 0 && input.length !==0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else if (input.length !==0) {
+          appendNoUser('該当するユーザーがいません');
+        }
+      })
+      .fail(function(){
+        alert('ユーザー検索に失敗しました');
+      })
+    });
+
+    $(document).on('click', '.user-search-add', function(){
+      var userName = $(this).data('user-name');
+      var userId = $(this).data('user-id');
+      appendToMember(userName, userId);
+      $(this).parent().remove();
+    });
+
+    $(document).on('click', '.user-search-remove', function(){
+      $(this).parent().remove();
+    });
+  });
+});
