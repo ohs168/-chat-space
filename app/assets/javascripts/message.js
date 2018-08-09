de07e236f37c534827d7cac5ad46bfe864ebc41e
shoji_hiroya <uncut8@shoujiyuuyanoMacBook-Pro.local>
2018-08-09 20:42:02 +0900

    Add-turbolinks

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,54 @@
-$(function() {
-  function buildHTML(message) {
-    var html =`<div class = "message">
-                <div class = "upper-message">
-                  <div class = "upper-message__user-name">
-                    ${ message.user_name }
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    function buildHTML(message) {
+      var html =`<div class = "message">
+                  <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                      ${ message.user_name }
+                    </div>
+                    <div class = "upper-message__date">
+                      ${ message.created_at }
+                    </div>
                   </div>
-                  <div class = "upper-message__date">
-                    ${ message.created_at }
+                  <div class = "lower-message">
+                    <div class = "lower-message__content">
+                      ${ message.content }
+                    </div>
+                    <div class = "lower-message__image">
+                      ${ message.image ? `<img src = '${ message.image }'>`: ""}
+                    </div>
                   </div>
-                </div>
-                <div class = "lower-message">
-                  <div class = "lower-message__content">
-                    ${ message.content }
-                  </div>
-                  <div class = "lower-message__image">
-                    ${ message.image ? `<img src = '${ message.image }'>`: ""}
-                  </div>
-                </div>
-              </div>`
-  return(html)
-  }
+                </div>`
+    return(html)
+    }
 
-  function scroll() {
-    $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight}, 'slow');
-  };
+    function scroll() {
+      $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight}, 'fast');
+    };
 
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(message) {
-      var html = buildHTML(message);
-      $('.messages').append(html);
-      $('#new_message')[0].reset();
-      scroll();
-    })
-    .fail(function(){
-      alert('メッセージを入力してください');
+    $('#new_message').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(message) {
+        var html = buildHTML(message);
+        $('.messages').append(html);
+        $('#new_message')[0].reset();
+        scroll();
+      })
+      .fail(function(){
+        alert('メッセージを入力してください');
+      });
+      return false;
     });
-    return false;
   });
 });
+
